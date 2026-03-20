54059a0f6f11f538ca37d0a83e757856f4cae999
eliassilva24151-wq <eliassilva24151@gmail.com>
2026-03-20 18:48:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -163,8 +163,8 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  root: path.resolve(import.meta.dirname, "client"),
-  publicDir: path.resolve(import.meta.dirname, "client", "public"),
+  root: path.resolve(import.meta.dirname, "./"),
+publicDir: path.resolve(import.meta.dirname, "public"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
